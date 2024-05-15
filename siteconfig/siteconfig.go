aa8ff5a0ab522d71370a267c5ae43ab7b727ce63
jeonghoikun.dev <jeonghoikun.dev@gmail.com>
2024-05-16 07:41:46 +0900

    description update

--- siteconfig/siteconfig.go
+++ siteconfig/siteconfig.go
@@ -61,8 +61,8 @@ var Config *config = &config{
 	DatePublished: time.Date(2024, time.Month(5), 14, 0, 0, 0, 0, time.Local),
 	DateModified:  time.Date(2024, time.Month(5), 15, 0, 0, 0, 0, time.Local),
 
-	Title:       "가즈아",
-	Description: "채우자",
+	Title:       "홍대 가라오케 가즈아",
+	Description: "홍대 가라오케 가즈아에서는 홍대 지역의 특색을 반영한 역동적이고 즐거운 분위기에서 친구들과 함께 신나는 노래방 경험을 즐길 수 있습니다. 가즈아는 젊은 층에게 인기 있는 장소로, 최신 곡들과 고품질의 사운드 시스템을 갖추고 있어 노래를 부르며 즐거운 시간을 보낼 수 있습니다. 활기찬 홍대의 밤문화를 대표하는 이곳에서 매력적인 음악과 함께 잊지 못할 추억을 만들어 보세요.",
 	Keywords:    "봉구실장, 가즈아 가라오케, 가즈아 봉구실장, 가라오케 봉구실장, 가라오케, 레츠고 봉구실장, 레츠고 가라오케",
 	Author:      "봉구실장",
 
